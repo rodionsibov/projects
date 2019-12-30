6c366ddf26d995da98fe778eedc97bc76e0d7ed1
voofer1 <voofer1@gmail.com>
2019-12-30 12:52:41 +0000

    add new files

---
+++ blurry-loading/app.js
@@ -0,0 +1,26 @@
+const loadText = document.querySelector('h1');
+const bg = document.querySelector('section');
+
+let load = 0;
+let int = setInterval(blurring, 30);
+
+function blurring() {
+    load++;
+    
+    if(load > 99) {
+	clearInterval(int);
+
+	setTimeout(() => {
+	    load = 0;
+	    int = setInterval(blurring, 30);
+	}, 2000);
+    }
+
+    loadText.innerText = load + '%';
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
