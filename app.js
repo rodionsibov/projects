441eaf470addd8c068358673bb8ead96a01d473f
rodion <rodionsibov@gmail.com>
2020-01-26 11:22:26 +0000

    update

---
+++ app.js
@@ -0,0 +1,32 @@
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+}
+
+    function ease(t, b, c, d) {
+	t /= d / 2;
+	if (t < 1) return c / 2 * t * t + b;
+	t--;
+	return -c / 2 * (t * (t-2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+var header = document.querySelector(".arrow");
+
+header.addEventListener("click", function() {
+    smoothScroll("section", 1000);
+});
+
+
