0b8e8e93a827ef2e6ad2d66c377e6c8da65ed22f
Rodion Sibov <rodionsibov@gmail.com>
2020-12-14 12:26:38 +0200

    Initial commit

---
+++ projectsInfo.js
@@ -0,0 +1,34 @@
+const projectsInfo = [
+    {
+	title: "Login Form | Bootstrap",
+	text: "This card has supporting text below as a natural lead-in to additional content.",
+	url: "login-form-bootstrap.html",
+	date: "Dec 14 2020 10:20",
+    },
+    {
+	title: "Complete Responsive Portfolio",
+	text: "This card has supporting text below as a natural lead-in to additional content.",
+	url: "complete-res-portfolio.html",
+	date: "Apr 12 2020 01:20",
+    },
+    {
+	title: "Background Changer",
+	text: "This card has supporting text below as a natural lead-in to additional content.",
+	url: "color-generator.html",
+	date: "Mar 11 2020 11:20",
+    },
+    {
+	title: "Quiz App",
+	text: "This card has supporting text below as a natural lead-in to additional content.",
+	url: "quiz-app.html",
+	date: "Dec 09 2020 11:20",
+    },
+    {
+	title: "Our Team Section",
+	text: "This card has supporting text below as a natural lead-in to additional content.",
+	url: "our-team-section.html",
+	date: "Feb 02 2020 11:20",
+    },
+]
+
+
