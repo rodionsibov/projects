71755ad6a8df63fa26b9070ddad694a6b83ba57b
Rodion Sibov <rodionsibov@gmail.com>
2020-12-14 16:27:10 +0200

    Update projectsInfo object

--- projectsInfo.js
+++ projectsInfo.js
@@ -1,34 +1,65 @@
 const projectsInfo = [
+
     {
 	title: "Login Form | Bootstrap",
-	text: "This card has supporting text below as a natural lead-in to additional content.",
+	text: "Basic login form HTML, CSS code through the Bootstrap Framework.", 
 	url: "login-form-bootstrap.html",
 	date: "Dec 14 2020 10:20",
     },
+
     {
 	title: "Complete Responsive Portfolio",
-	text: "This card has supporting text below as a natural lead-in to additional content.",
+	text: "One Page Full Website Project For Practice. HTML & CSS Responsive Website.",
 	url: "complete-res-portfolio.html",
 	date: "Apr 12 2020 01:20",
     },
+
     {
 	title: "Background Changer",
 	text: "This card has supporting text below as a natural lead-in to additional content.",
 	url: "color-generator.html",
 	date: "Mar 11 2020 11:20",
     },
+
     {
 	title: "Quiz App",
-	text: "This card has supporting text below as a natural lead-in to additional content.",
+	text: "Create a Quiz App with using HTML CSS & JavaScript",
 	url: "quiz-app.html",
-	date: "Dec 09 2020 11:20",
+	date: "Nov 29 2020 23:30",
     },
+
     {
 	title: "Our Team Section",
-	text: "This card has supporting text below as a natural lead-in to additional content.",
+	text: "Sections are often used to list the employers in a firm with contact info.",
 	url: "our-team-section.html",
 	date: "Feb 02 2020 11:20",
     },
+
+    {
+	title: "CSS Background Clip Text",
+	text: "It allows a background image to be clipped by a text element.",
+	url: "bg-clip-text.html",
+	date: "Jun 16 2020 23:28",
+    },
+
+    {
+	title: "Green Button",
+	text: "Create and style a Button with the HTML Button Element. Use a hover effect.",
+	url: "button.html",
+	date: "Jun 16 2020 20:21",
+    },
+
+    {
+	title: "Progress Bar",
+	text: "Create and style a Button with the HTML Button Element. Use a hover effect.",
+	url: "progress-bar.html",
+	date: "Jun 16 2020 20:21",
+    },
+
+    {
+	title: "Weight Converter App",
+	text: "This calculator can convert values between the common weight and mass units.",
+	url: "weight-converter-app.html",
+	date: "Jun 16 2020 10:20",
+    },
 ]
-
-
