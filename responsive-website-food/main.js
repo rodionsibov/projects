be6763b5540b9b2268b8e2f1043389673737434b
Rodion Sibov <rodionsibov@gmail.com>
2021-08-17 15:27:14 +0300

    feat: change background header

--- responsive-website-food/main.js
+++ responsive-website-food/main.js
@@ -34,3 +34,11 @@ function scrollActive() {
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+function scrollHeader() {
+    const nav = document.querySelector('#header')
+    if(this.scrollY >= 200) nav.classList.add('scroll-header')
+    else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
