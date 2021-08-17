1478909a307df7636ae640ef12787bf3024dcfa7
Rodion Sibov <rodionsibov@gmail.com>
2021-08-17 15:20:54 +0300

    feat: scroll sections active link

--- responsive-website-food/main.js
+++ responsive-website-food/main.js
@@ -1,7 +1,7 @@
 const showMenu = (toggleId, navId) => {
     const toggle = document.querySelector(`#${toggleId}`)
     const nav = document.querySelector(`#${navId}`)
-    if(toggle && nav) {
+    if (toggle && nav) {
         toggle.addEventListener('click', () => {
             nav.classList.toggle('show-menu')
         })
@@ -18,3 +18,19 @@ function linkAction() {
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+const sections = document.querySelectorAll('section[id]')
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
