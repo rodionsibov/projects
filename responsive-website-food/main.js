ab75e80798cf63c8f326e7f835aa19f75772c017
Rodion Sibov <rodionsibov@gmail.com>
2021-08-17 14:57:28 +0300

    feat: show menu & remove menu mobile

--- responsive-website-food/main.js
+++ responsive-website-food/main.js
@@ -8,3 +8,13 @@ const showMenu = (toggleId, navId) => {
     }
 }
 showMenu('nav-toggle', 'nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.querySelector('#nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
