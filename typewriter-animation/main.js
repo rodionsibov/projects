07ea0fa9f4507f87d4652f3f6b772df4e61caec0
Rodion Sibov <rodionsibov@gmail.com>
2021-09-21 10:34:37 +0300

    feat: determine the total number of chars in the string

--- typewriter-animation/main.js
+++ typewriter-animation/main.js
@@ -1,4 +1,5 @@
 function typingAnimation() {
     let textElement = document.querySelector(".text");
     let textArray = textElement.innerHTML.split("");
+    let allWords = textElement.innerHTML.split(" ");
 }
