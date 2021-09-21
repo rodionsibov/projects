b342324d82cc640e70c1522b9a69949d918f6125
Rodion Sibov <rodionsibov@gmail.com>
2021-09-21 10:45:15 +0300

    feat: calculate the length of the entire string as well as the length of each and every individual word

--- typewriter-animation/main.js
+++ typewriter-animation/main.js
@@ -2,4 +2,9 @@ function typingAnimation() {
     let textElement = document.querySelector(".text");
     let textArray = textElement.innerHTML.split("");
     let allWords = textElement.innerHTML.split(" ");
+    let textLen = textArray.length;
+
+    const wordLen = allWords.map((word) => {
+        return word.length;
+    });
 }
