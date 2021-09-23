96bdc75f78812bc509d4964ac9f9f7469dff2b82
Rodion Sibov <rodionsibov@gmail.com>
2021-09-23 13:28:44 +0300

    feat: add animation

--- responsive-website-food/main.js
+++ responsive-website-food/main.js
@@ -69,3 +69,15 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+// scroll reveal animation 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal(`.home__data, .home__img, .about__data, .about__img, .services__content, .menu__content, .app__data, .app__img, .contact__data, .contact__button, .footer__content`, {
+    interval: 200
+})
