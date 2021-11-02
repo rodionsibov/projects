b22a49355e6d191d9e20e8753714b11695e37726
Rodion Sibov <rodionsibov@gmail.com>
2021-11-02 20:04:16 +0200

    update

--- qr-code-generator/main.js
+++ qr-code-generator/main.js
@@ -1,12 +1,11 @@
 const input = document.querySelector('.form input')
 const button = document.querySelector('.form button')
 const qrImg = document.querySelector('.qr-img img')
-
 button.addEventListener('click', () => {
     let data = input.value
     if (data.length > 0) {
         let imgSrc = `https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=${data}`
         qrImg.src = imgSrc
-       data = ''
+        data = ''
     }
 })
